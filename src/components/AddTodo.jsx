b6bce00abffd33bda43d6f70565c7ebd44508564
satya960 <satya9604166@gmail.com>
2025-02-26 12:32:49 +0530

    first commit

---
+++ src/components/AddTodo.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addTodo } from "../context/todoSlice";
+
+function AddTodo() {
+  const [todoName, setTodoName] = useState("");
+  const [todoDate, setTodoDate] = useState("");
+  const dispatch = useDispatch();
+
+  const handleAddTodo = () => {
+    if (todoName && todoDate) {
+      dispatch(addTodo({ id: Date.now(), todoName, todoDate }));
+      setTodoName("");
+      setTodoDate("");
+    }
+  };
+
+  return (
+    <div className="bg-white  dark:bg-gray-800 shadow-lg rounded-xl p-6 mb-6">
+      <div className="flex flex-col md:flex-row items-center gap-6">
+        <div className="w-full md:w-1/2">
+          <input
+            type="text"
+            placeholder="Enter todo here"
+            value={todoName}
+            onChange={(e) => setTodoName(e.target.value)}
+            className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 transition-colors flex-wrap"
+          />
+        </div>
+
+        <div className="w-full md:w-1/3">
+            <input
+              id="todoDate"
+              type="date"
+              value={todoDate}
+              onChange={(e) => setTodoDate(e.target.value)} 
+              onFocus={(e) => e.target.showPicker()} 
+              className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 transition-colors"
+            />
+        </div>
+
+        <div className="w-full md:w-1/3 ">
+          <button
+            type="button"
+            onClick={handleAddTodo}
+            className="w-full  bg-green-500 hover:bg-green-600 text-white font-bold py-3 px-6 rounded-full transition duration-200"
+          >
+            Add
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AddTodo;
---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,23 @@
+function TodoItem({ todoName, todoDate, onDelete }) {
+  return (
+    <div className="flex flex-row items-center justify-between bg-white dark:bg-gray-800 shadow-md hover:shadow-lg transition-shadow duration-300 rounded-lg p-4 mb-4">
+      <div className="flex-1 md:w-1/2 text-gray-800 dark:text-gray-100 font-medium flex-wrap">
+        {todoName}
+      </div>
+      <div className="flex-1  md:w-1/3 text-gray-600 dark:text-gray-300 font-medium ">
+        {todoDate}
+      </div>
+      <div className="mt-2 md:mt-0 ">
+        <button
+          type="button"
+          onClick={onDelete}
+          className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full transition-colors duration-200"
+        >
+          Delete
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default TodoItem;
