4e24243e006166a6e8281c93dd787bbd48f2afa5
satya960 <satya9604166@gmail.com>
2025-02-26 22:52:07 +0530

    Fixed base path for GitHub Pages

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -2,11 +2,11 @@ function TodoItem({ todoName, todoDate, onDelete }) {
   return (
     <div className="flex flex-col md:flex-row items-center justify-between shadow-md hover:shadow-lg transition-shadow duration-300 rounded-lg p-4 mb-4">
       <div className="w-full md:w-1/2  font-medium break-words whitespace-normal">
-        {todoName}
+        <b className="font-sans">{todoName}</b>
       </div>
 
       <div className="w-full md:w-1/3  font-medium text-center mt-2 md:mt-0">
-        {todoDate}
+        <b className="font-black">{todoDate}</b>
       </div>
 
       <div className="w-full md:w-auto mt-2 md:mt-0">
